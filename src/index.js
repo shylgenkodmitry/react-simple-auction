406d4458fbe999484857dd850009ed9b1d7d978d
Dmitry Shylgenko <shylgenko.dmitry@gmail.com>
2019-05-21 21:30:00 +0300

    Handling google fonts

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './assets/css/bootstrap.min.css';
+import './assets/css/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/animate.css';
+
+import App from './App';
+
 import './assets/css/style.css';
 import './assets/css/custom.css';
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
